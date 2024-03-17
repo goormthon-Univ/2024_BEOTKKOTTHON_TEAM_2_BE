67af7ad5f55cb8b531d5beab4b5ad0ee66bdb7ad
sgh4123@naver.com <sgh4123@naver.com>
2024-03-18 02:51:20 +0900

    수정

--- muckatlist_nest/src/main.ts
+++ muckatlist_nest/src/main.ts
@@ -12,17 +12,13 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
 
-  // Swagger 문서 생성 및 노출
   const document = SwaggerModule.createDocument(app, options);
   app.use('/swagger-json', (req, res) => {
     res.json(document);
   });
 
-  // SwaggerService를 사용하여 서버들의 Swagger JSON을 가져와 병합
-  // 병합된 Swagger JSON을 Swagger UI에 설정
   SwaggerModule.setup('swagger-ui', app, document);
 
-  // 앱 실행
   await app.listen(3000);
 }
 
