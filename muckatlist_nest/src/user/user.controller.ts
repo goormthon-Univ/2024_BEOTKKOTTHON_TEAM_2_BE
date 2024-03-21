09f22aef975550bb79ed087fdf926227494fa5ad
sgh4123@naver.com <sgh4123@naver.com>
2024-03-22 03:53:28 +0900

    user컨트롤러 수정

--- muckatlist_nest/src/user/user.controller.ts
+++ muckatlist_nest/src/user/user.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
 import { PatchUserDto, PostUserDto } from './dto';
+import { throwErrorHttp } from 'src/utils';
 
 @ApiTags('유저')
 @Controller('/api/user')
@@ -9,32 +10,58 @@ export class UserController {
     constructor(private readonly userService: UserService){}
 
     @ApiOperation({ summary: '로그인'})
+    @HttpCode(200)
     @Post('/login')
     async login(@Body('kakao_Id') kakao_Id: string){
-
+        try{
+            return await this.userService.login(kakao_Id);
+        }
+        catch (error) {
+            throwErrorHttp(error);
+        }
     }
 
     @ApiOperation({ summary: '회원가입'})
     @Post('/register')
     async register(@Body() postUserDto: PostUserDto){
-
+        try{
+            return await this.userService.saveUser(postUserDto);
+        }
+        catch (error) {
+            throwErrorHttp(error);
+        }
     }
 
     @ApiOperation({ summary: '사용자 정보 조회'})
     @Get('/:kakao_Id')
     async getUserInformation(@Param('kakao_Id') kakao_Id: string){
-
+        try{
+            return await this.userService.getUserById(kakao_Id);
+        }
+        catch (error) {
+            throwErrorHttp(error);
+        }
     }
 
     @ApiOperation({ summary: '사용자 정보 수정(포인트, 닉네임, 학교정보, fcm_Token 포함)'})
     @Patch()
     async patchUserInformation(@Body() patchUserDto: PatchUserDto){
-
+        try{
+            return await this.userService.modifyUser(patchUserDto);
+        }
+        catch (error) {
+            throwErrorHttp(error);
+        }
     }
 
     @ApiOperation({ summary: '탈퇴'})
     @Delete('/:kakao_Id')
     async deleteUserInformation(@Param('kakao_Id') kakao_Id: string){
-
+        try{
+            return await this.userService.removeUser(kakao_Id);
+        }
+        catch (error) {
+            throwErrorHttp(error);
+        }
     }
 }
--- muckatlist_nest/src/user/user.service.ts
+++ muckatlist_nest/src/user/user.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
-
+import { PatchUserDto, PostUserDto } from './dto';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 @Injectable()
-export class UserService {}
+export class UserService {
+    async login(kakao_Id: string) {
+        const user = await prisma.user_info.findUnique({
+            where: {
+                kakao_id: kakao_Id
+            },
+        })
+        throw new Error('Method not implemented.');
+    }
+    async removeUser(kakao_Id: string) {
+        throw new Error('Method not implemented.');
+    }
+    async modifyUser(patchUserDto: PatchUserDto) {
+        throw new Error('Method not implemented.');
+    }
+    async getUserById(kakao_Id: string) {
+        throw new Error('Method not implemented.');
+    }
+    async saveUser(postUserDto: PostUserDto) {
+        throw new Error('Method not implemented.');
+    }
+}
